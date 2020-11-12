c469c9602e7657114043195f5b2857864757e78e
catnoodle21 <fiftyshadesofputin@gmail.com>
2020-11-12 20:15:47 +1000

    first commit

---
+++ WebApplication1/Models/UserView.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class UserView
+    {
+        public int Id { get; set; }
+        public string Fname { get; set; }
+        public string Sname { get; set; }
+        public virtual DepartmentView Department { get; set; }
+
+        public static implicit operator UserView(Users users)
+        {
+            if (users == null) { return new UserView(); }
+
+            return new UserView
+            {
+                Id = users.Id,
+                Fname = users.Fname,
+                Sname = users.Sname,
+                Department = users.IdDepartmentNavigation
+            };
+        }
+    }
+}
